edb79b4d0bfb12aa380ed55ba9f1fe6945aa8189
lyynb666nb <3180033202@qq.com>
2026-02-11 18:13:20 +0800

    Add files via upload

---
+++ admin.lua
@@ -0,0 +1,5 @@
+local msg = Instance.new("Message",workspace)
+msg.Text = "Admin Yttrium 管理员版本"
+wait(2)
+msg:Destroy()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lyynb666nb/lyynb/refs/heads/main/YG%E5%BC%80%E6%BA%90ohio.lua"))()
